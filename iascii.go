bede32be14043b75d1670bbc81df275ebaebef72
Leland Miller <lelandmiller@gmail.com>
2015-03-12 22:25:58 -0700

    Made to work with go version go1.4.2.

--- iascii.go
+++ iascii.go
@@ -2,23 +2,25 @@
 package iascii
 
 import (
+	"errors"
 	"image"
+	"image/color"
 	"io"
-	"os"
 )
 
-func Encode(w io.Writer, m image.Image) (err os.Error) {
+func Encode(w io.Writer, m image.Image) (err error) {
 	b := m.Bounds()
 	mw, mh := b.Dx(), b.Dy()
 	if mw <= 0 || mh <= 0 {
-		err = os.NewError("Bad image bounds")
+		err = errors.New("Bad image bounds")
 		return
 	}
 
 	cm := []byte(".ocOGDQ@")
 	for y := b.Min.Y; y < b.Max.Y; y++ {
 		for x := b.Min.X; x < b.Max.X; x++ {
-			c := image.GrayColorModel.Convert(m.At(x, y)).(image.GrayColor)
+			c := color.GrayModel.Convert(m.At(x, y)).(color.Gray)
+			//.(image.GrayColor)
 			c.Y >>= 5
 			_, err = w.Write(cm[c.Y : c.Y+1])
 			if err != nil {
